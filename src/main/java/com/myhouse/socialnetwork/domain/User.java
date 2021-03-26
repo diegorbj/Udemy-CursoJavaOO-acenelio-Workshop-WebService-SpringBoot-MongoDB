35da58b7dd5a47805721ee2e5553607737adec2f
Diego Barros <diegorbj@outlook.com>
2021-03-26 16:43:23 -0300

    Referenciando os posts do usuário

--- src/main/java/com/myhouse/socialnetwork/domain/User.java
+++ src/main/java/com/myhouse/socialnetwork/domain/User.java
@@ -2,12 +2,15 @@ package com.myhouse.socialnetwork.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
+@Getter
+@Setter
 @ToString
 @NoArgsConstructor
 @Document
@@ -15,22 +18,14 @@ public class User implements Serializable {
 
     private final static long serialVersionUID = 1L;
 
-    @Getter
-    @Setter
     @EqualsAndHashCode.Include
     @Id
     private String id;
-
-    @Getter
-    @Setter
     private String name;
-
-    @Getter
-    @Setter
     private String email;
 
-    @Getter
-    private Set<Post> posts = new HashSet<>();
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User(String id, String name, String email) {
         this.id = id;
